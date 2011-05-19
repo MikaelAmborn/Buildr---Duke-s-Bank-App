14caf225d94557542d04de31b884f0974662f1cd
Mikael Amborn <mikael.amborn@gmail.com>
2011-05-19 20:12:08 +0200

    Cleaned up and fixed sql dates

--- sql/hsql-insert.sql
+++ sql/hsql-insert.sql
@@ -61,85 +61,85 @@ INSERT INTO customer_account_xref VALUES
 ('201', '5008');
 
 INSERT INTO tx VALUES
-('1', '5005', '2003-9-01 12:55:33', 200.00, 4200.00, 'Refund');
+('1', '5005', '2003-09-01 12:55:33', 200.00, 4200.00, 'Refund');
 UPDATE account SET balance = 4200.00 WHERE account_id = '5005';
 INSERT INTO tx VALUES
-('3', '5008', '2003-9-03 12:56:33', -1000.00, 54601.35, 'Transfer Out');
+('3', '5008', '2003-09-03 12:56:33', -1000.00, 54601.35, 'Transfer Out');
 UPDATE account SET balance = 54604.35 WHERE account_id = '5008'; 
 INSERT INTO tx VALUES
-('4', '5006', '2003-9-03 12:57:33', 1000.00, 1085.00, 'Transfer In');
+('4', '5006', '2003-09-03 12:57:33', 1000.00, 1085.00, 'Transfer In');
 UPDATE account SET balance = 1085.00 WHERE account_id = '5006'; 
 INSERT INTO tx VALUES
-('5', '5007', '2003-9-05 12:58:33', 33.00, 199.08, 'Clothing');
+('5', '5007', '2003-09-05 12:58:33', 33.00, 199.08, 'Clothing');
 UPDATE account SET balance = 199.08 WHERE account_id = '5007'; 
 INSERT INTO tx VALUES
-('6', '5006', '2003-9-06 12:59:33', 2000.00, 3085.00, 'Paycheck Deposit');
+('6', '5006', '2003-09-06 12:59:33', 2000.00, 3085.00, 'Paycheck Deposit');
 UPDATE account SET balance = 3085.00 WHERE account_id = '5006'; 
 INSERT INTO tx VALUES
-('7', '5005', '2003-9-07 13:00:33', -200.00, 4000.00, 'ATM Withdrawal');
+('7', '5005', '2003-09-07 13:00:33', -200.00, 4000.00, 'ATM Withdrawal');
 UPDATE account SET balance = 3085.00 WHERE account_id = '5005'; 
 INSERT INTO tx VALUES
-('8', '5006', '2003-9-08 13:01:33', -200.00, 2885.00, 'Car Insurance');
+('8', '5006', '2003-09-08 13:01:33', -200.00, 2885.00, 'Car Insurance');
 UPDATE account SET balance = 2885.00 WHERE account_id = '5006'; 
 INSERT INTO tx VALUES
-('9', '5007', '2003-9-09 13:02:33', 186.00, 385.08, 'Car Repair');
+('9', '5007', '2003-09-09 13:02:33', 186.00, 385.08, 'Car Repair');
 UPDATE account SET balance = 385.08 WHERE account_id = '5007'; 
 INSERT INTO tx VALUES
-('10', '5008', '2003-9-10 12:55:33', 1000.00, 55601.35, 'Deposit');
+('10', '5008', '2003-09-10 12:55:33', 1000.00, 55601.35, 'Deposit');
 UPDATE account SET balance = 55601.35 WHERE account_id = '5008'; 
 INSERT INTO tx VALUES
-('11', '5007', '2003-9-11 12:55:33', 585.00, 970.08, 'Airplane Tickets');
+('11', '5007', '2003-09-11 12:55:33', 585.00, 970.08, 'Airplane Tickets');
 UPDATE account SET balance = 970.08 WHERE account_id = '5007'; 
 INSERT INTO tx VALUES
-('12', '5006', '2003-9-12 12:55:33', -675.00, 2210.00, 'Mortgage Payment');
+('12', '5006', '2003-09-12 12:55:33', -675.00, 2210.00, 'Mortgage Payment');
 UPDATE account SET balance = 2210.00 WHERE account_id = '5006'; 
 INSERT INTO tx VALUES
-('13', '5005', '2003-9-13 12:55:33', -100.00, 3900.00, 'ATM Withdrawal');
+('13', '5005', '2003-09-13 12:55:33', -100.00, 3900.00, 'ATM Withdrawal');
 UPDATE account SET balance = 3900.00 WHERE account_id = '5005'; 
 INSERT INTO tx VALUES
-('14', '5006', '2003-9-14 12:55:33', -385.08, 1824.92, 'Visa Payment');
+('14', '5006', '2003-09-14 12:55:33', -385.08, 1824.92, 'Visa Payment');
 UPDATE account SET balance = 1824.92 WHERE account_id = '5006'; 
 INSERT INTO tx VALUES
-('15', '5007', '2003-9-15 12:55:33', -385.08, 585.00, 'Payment');
+('15', '5007', '2003-09-15 12:55:33', -385.08, 585.00, 'Payment');
 UPDATE account SET balance = 585.00 WHERE account_id = '5007'; 
 INSERT INTO tx VALUES
-('17', '5007', '2003-9-17 12:55:33', 26.95, 611.95, 'Movies');
+('17', '5007', '2003-09-17 12:55:33', 26.95, 611.95, 'Movies');
 UPDATE account SET balance = 611.95 WHERE account_id = '5007'; 
 INSERT INTO tx VALUES
-('18', '5006', '2003-9-18 12:55:33', -31.00, 1793.92, 'Groceries');
+('18', '5006', '2003-09-18 12:55:33', -31.00, 1793.92, 'Groceries');
 UPDATE account SET balance = 1793.92 WHERE account_id = '5006'; 
 INSERT INTO tx VALUES
-('19', '5005', '2003-9-19 12:55:33', -150.00, 3750.00, 'ATM Withdrawal');
+('19', '5005', '2003-09-19 12:55:33', -150.00, 3750.00, 'ATM Withdrawal');
 UPDATE account SET balance = 3750.00 WHERE account_id = '5005'; 
 INSERT INTO tx VALUES
-('20', '5006', '2003-9-20 12:55:33', 2000.00, 3173.92, 'Paycheck Deposit');
+('20', '5006', '2003-09-20 12:55:33', 2000.00, 3173.92, 'Paycheck Deposit');
 UPDATE account SET balance = 3173.92 WHERE account_id = '5006'; 
 INSERT INTO tx VALUES
-('21', '5007', '2003-9-21 12:55:33', 124.00, 735.95, 'Furnishings');
+('21', '5007', '2003-09-21 12:55:33', 124.00, 735.95, 'Furnishings');
 UPDATE account SET balance = 735.95 WHERE account_id = '5007'; 
 INSERT INTO tx VALUES
-('23', '5007', '2003-9-23 12:55:33', 33.12, 769.07, 'Hardware');
+('23', '5007', '2003-09-23 12:55:33', 33.12, 769.07, 'Hardware');
 UPDATE account SET balance = 769.07 WHERE account_id = '5007'; 
 INSERT INTO tx VALUES
-('24', '5006', '2003-9-24 12:55:33', -175.33, 2998.59, 'Utility Bill');
+('24', '5006', '2003-09-24 12:55:33', -175.33, 2998.59, 'Utility Bill');
 UPDATE account SET balance = 2998.59 WHERE account_id = '5006'; 
 INSERT INTO tx VALUES
-('25', '5006', '2003-9-25 12:55:33', -123.00, 2875.59, 'Groceries');
+('25', '5006', '2003-09-25 12:55:33', -123.00, 2875.59, 'Groceries');
 UPDATE account SET balance = 2875.59 WHERE account_id = '5006'; 
 INSERT INTO tx VALUES
-('26', '5006', '2003-9-26 12:55:33', -675.00, 2200.59, 'Mortgage Payment');
+('26', '5006', '2003-09-26 12:55:33', -675.00, 2200.59, 'Mortgage Payment');
 UPDATE account SET balance = 2200.59 WHERE account_id = '5006'; 
 INSERT INTO tx VALUES
-('27', '5007', '2003-9-27 12:55:33', 24.72, 793.79, 'Cafe');
+('27', '5007', '2003-09-27 12:55:33', 24.72, 793.79, 'Cafe');
 UPDATE account SET balance = 793.79 WHERE account_id = '5007'; 
 INSERT INTO tx VALUES
-('28', '5008', '2003-9-28 12:55:33', 1000.00, 56601.35, 'Deposit');
+('28', '5008', '2003-09-28 12:55:33', 1000.00, 56601.35, 'Deposit');
 UPDATE account SET balance = 56601.35 WHERE account_id = '5008'; 
 INSERT INTO tx VALUES
-('29', '5007', '2003-9-29 12:55:33', 35.00, 828.79, 'Hair Salon');
+('29', '5007', '2003-09-29 12:55:33', 35.00, 828.79, 'Hair Salon');
 UPDATE account SET balance = 828.79 WHERE account_id = '5007'; 
 INSERT INTO tx VALUES
-('30', '5006', '2003-9-30 12:55:33', -20.00, 2180.59, 'Gasoline');
+('30', '5006', '2003-09-30 12:55:33', -20.00, 2180.59, 'Gasoline');
 UPDATE account SET balance = 2180.59 WHERE account_id = '5006'; 
 INSERT INTO tx VALUES
 ('31', '5005', '2003-10-01 12:55:33', -100.00, 3650.00, 'ATM Withdrawal');
@@ -273,4 +273,3 @@ UPDATE account SET balance = 220.03 WHERE account_id = '5007';
 INSERT INTO tx VALUES
 ('86', '5005', '2003-11-23 12:55:33', -100.00, 3300.00, 'ATM Withdrawal');
 UPDATE account SET balance = 3300.00 WHERE account_id = '5005'; 
-
